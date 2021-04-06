d6f9578d57b6e732af82eb5537a49eb523152492
MARÍA MARTÍNEZ GÓMEZ, MARÍA <mariamg2003@gmail.com>
2021-04-06 17:49:29 +0200

    Añadir comentario a FirstFragment.java

--- app/src/main/java/com/example/diechichat/FirstFragment.java
+++ app/src/main/java/com/example/diechichat/FirstFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
+    //hola esto es un comentario
 
     @Override
     public View onCreateView(
