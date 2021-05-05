c4097865f54659621c29f8ed4e5ca9a2b71246da
marta-pvfpig <82042082+marta-pvfpig@users.noreply.github.com>
2021-05-05 16:53:40 +0200

    Update DlgConfirmacion.java

--- app/src/main/java/com/example/diechichat/vista/dialogos/DlgConfirmacion.java
+++ app/src/main/java/com/example/diechichat/vista/dialogos/DlgConfirmacion.java
@@ -61,8 +61,8 @@ public class DlgConfirmacion extends DialogFragment {
     }
 
     /* The activity that creates an instance of this dialog fragment must
-         * implement this interface in order to receive event callbacks.
-         * Each method passes the DialogFragment in case the host needs to query it. */
+         * implement this interface in order to receive event callbacks.
+         * Each method passes the DialogFragment in case the host needs to query it. */
     public interface DlgConfirmacionListener {
         void onDlgConfirmacionPositiveClick(DialogFragment dialog);
 
