9ccc2b9236051249a168b9bdcda911fe107364d8
marta-pvfpig <82042082+marta-pvfpig@users.noreply.github.com>
2021-05-05 18:10:59 +0200

    Update MisClientesActivity.java

--- app/src/main/java/com/example/diechichat/vista/MisClientesActivity.java
+++ app/src/main/java/com/example/diechichat/vista/MisClientesActivity.java
@@ -2,14 +2,10 @@ package com.example.diechichat.vista;
 
 import android.os.Bundle;
 
-import com.example.diechichat.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.example.diechichat.R;
 
 public class MisClientesActivity extends AppCompatActivity {
 
