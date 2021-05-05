7498fd6cd0ea9673f353165ef345be6dbdf27cb1
marta-pvfpig <82042082+marta-pvfpig@users.noreply.github.com>
2021-05-05 17:11:38 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/diechichat/MainActivity.java
+++ app/src/main/java/com/example/diechichat/MainActivity.java
@@ -10,20 +10,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.navigation.NavController;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.diechichat.R;
 import com.example.diechichat.databinding.ActivityMainBinding;
 import com.example.diechichat.databinding.AppBarMainBinding;
+import com.example.diechichat.modelo.Cliente;
+import com.example.diechichat.modelo.Nutricionista;
 import com.example.diechichat.vista.dialogos.DlgConfirmacion;
 import com.example.diechichat.vista.fragmentos.MiPerfilFragment;
+import com.example.diechichat.vista.fragmentos.LoginFragment;
+import com.example.diechichat.vistamodelo.LoginViewModel;
+import com.example.diechichat.vistamodelo.MainViewModel;
+import com.example.diechichat.vistamodelo.NutriViewModel;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements
+        LoginFragment.LoginFragInterface,
         DlgConfirmacion.DlgConfirmacionListener {
 
-
+    private MainViewModel mainVM;
+    private LoginViewModel loginVM;
+    private NutriViewModel nutriVM;
+                
     private ActivityMainBinding bindingMain;
     private AppBarMainBinding bindingAppBar;
 
@@ -33,21 +46,18 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+            
         bindingMain =ActivityMainBinding.inflate(getLayoutInflater());
         bindingAppBar=bindingMain.appBarMain;
         setContentView(bindingMain.getRoot());
         setSupportActionBar(bindingAppBar.mainToolbar);
 
         mNavC = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragCV)).getNavController();
-        mAppBarConfiguration= new AppBarConfiguration.Builder(mNavC.getGraph()).
-                setOpenableLayout(bindingMain.drawerLayout).
-                build();
+        mAppBarConfiguration= new AppBarConfiguration.Builder(mNavC.getGraph()).setOpenableLayout(bindingMain.drawerLayout).build();
         NavigationUI.setupActionBarWithNavController(this,mNavC,mAppBarConfiguration);
         bindingMain.navView.setNavigationItemSelectedListener(navView_OnNavigationItemSelected);
 
+        mainVM = new ViewModelProvider(this).get(MainViewModel.class);
     }
 
     @Override
@@ -59,9 +69,9 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public boolean onSupportNavigateUp() {
-        return NavigationUI.navigateUp(mNavC, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(mNavC, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
+                
     private void mostrarDlgSalir(){
         //Lanzamos DlgConfirmacion
         Bundle bundle = new Bundle();
@@ -70,7 +80,6 @@ public class MainActivity extends AppCompatActivity implements
         bundle.putString("tag", "tagConfirmacion_Salir");
         mNavC.navigate(R.id.action_global_dlgConfirmacionMain, bundle);
     }
-
 
     @Override
     public void onDlgConfirmacionPositiveClick(DialogFragment dialog) {
@@ -95,6 +104,7 @@ public class MainActivity extends AppCompatActivity implements
     protected void onDestroy() {
         super.onDestroy();
     }
+                
     private final NavigationView.OnNavigationItemSelectedListener navView_OnNavigationItemSelected = new NavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -116,4 +126,37 @@ public class MainActivity extends AppCompatActivity implements
             return false;
         }
     };
+                
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Métodos login
+
+    @Override
+    public void onEntrarLoginFrag(Object obj) {
+        if(obj != null) {
+            mainVM.setLogin(obj);
+            if (obj instanceof Nutricionista) {
+                Snackbar.make(bindingMain.getRoot(), "Estás dentro como administrador", Snackbar.LENGTH_SHORT).show();
+            } else if (obj instanceof Cliente) {
+                Snackbar.make(bindingMain.getRoot(), "Estás dentro como cliente", Snackbar.LENGTH_SHORT).show();
+            }
+            finish();
+            mNavC.navigateUp();
+        } else {
+            Snackbar.make(bindingMain.getRoot(), "Introduce usuario y contraseña", Snackbar.LENGTH_SHORT).show();
+        }
+    }
 }
