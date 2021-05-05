c24ea1eba738b73a6ee178c6cf4f3bc83cd7f710
marta-pvfpig <82042082+marta-pvfpig@users.noreply.github.com>
2021-05-05 18:09:16 +0200

    Update MiPerfilActivity.java

--- app/src/main/java/com/example/diechichat/vista/MiPerfilActivity.java
+++ app/src/main/java/com/example/diechichat/vista/MiPerfilActivity.java
@@ -1,21 +1,16 @@
 package com.example.diechichat.vista;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.diechichat.R;
 import com.example.diechichat.databinding.ActivityMiPerfilBinding;
 import com.example.diechichat.vista.fragmentos.MiPerfilFragment;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-
-import android.view.MenuItem;
-import android.view.View;
 
 public class MiPerfilActivity extends AppCompatActivity implements MiPerfilFragment.PerfilFragInterface {
 
@@ -25,13 +20,13 @@ public class MiPerfilActivity extends AppCompatActivity implements MiPerfilFragm
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityMiPerfilBinding.inflate(getLayoutInflater());
+        binding = ActivityMiPerfilBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
-        if(getSupportActionBar()!=null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        mNavC=((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.miperfilFragCV)).getNavController();
+        mNavC = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.miperfilFragCV)).getNavController();
 //        setContentView(R.layout.activity_mi_perfil);
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -45,7 +40,7 @@ public class MiPerfilActivity extends AppCompatActivity implements MiPerfilFragm
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.menu_miperfil){
+        if (item.getItemId() == R.id.menu_miperfil) {
 
         }
         return super.onOptionsItemSelected(item);
