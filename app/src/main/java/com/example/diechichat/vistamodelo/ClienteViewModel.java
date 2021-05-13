62a6444232e95082f89f9ee66de9a5d732ca5322
marta-pvfpig <82042082+marta-pvfpig@users.noreply.github.com>
2021-05-13 22:19:07 +0200

    Update ClienteViewModel.java

--- app/src/main/java/com/example/diechichat/vistamodelo/ClienteViewModel.java
+++ app/src/main/java/com/example/diechichat/vistamodelo/ClienteViewModel.java
@@ -43,7 +43,7 @@ public class ClienteViewModel extends AndroidViewModel {
         return mCliente;
     }
 
-    public LiveData<Boolean> altaNutricionista(Cliente cli) {
+    public LiveData<Boolean> altaCliente(Cliente cli) {
         return mCliRep.altaCliente(cli);
     }
 
