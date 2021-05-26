4d630a49d3d7f5282973372bf5ca9c4e736242d6
marta-pvfpig <696919@alu.murciaeduca.es>
2021-05-26 19:20:08 +0200

    actualización

--- app/src/main/java/com/example/diechichat/vista/fragmentos/DietaFragment.java
+++ app/src/main/java/com/example/diechichat/vista/fragmentos/DietaFragment.java
@@ -1,7 +1,10 @@
 package com.example.diechichat.vista.fragmentos;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,13 +12,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.diechichat.R;
+import com.example.diechichat.databinding.FragmentDietaBinding;
+import com.example.diechichat.databinding.FragmentNuevoClienteBinding;
+import com.example.diechichat.modelo.Cliente;
+
+import org.jetbrains.annotations.NotNull;
 
 public class DietaFragment extends Fragment {
+
+    private FragmentDietaBinding binding;
+    private DietaFragment.DietaFragmentInterface mListener;
 
     public DietaFragment() {
         // Required empty public constructor
     }
 
+
+    public interface DietaFragmentInterface {
+        void onAsignarDieta();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof NuevoCienteFragment.NuevoCliFragmentInterface) {
+            mListener = (DietaFragment.DietaFragmentInterface) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement DietaFragmentInterface");
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +51,28 @@ public class DietaFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dieta, container, false);
+        binding = FragmentDietaBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
     }
 }
